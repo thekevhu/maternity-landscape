f591bc0241759680c297000c7944a533e207a73c
Kevin Hu <kevinhu@Kevins-MacBook-Air.local>
2023-10-16 12:50:34 -0400

    Updating Dataset

---
+++ build/precache-manifest.0cf77fd8ca8120d16efc5b6872d5d0c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc73147e9605e00cf9422a25c0a0862",
+    "url": "/maternity-landscape/index.html"
+  },
+  {
+    "revision": "96b85323f48b1c60dc5d",
+    "url": "/maternity-landscape/static/css/main.ba5a5e71.chunk.css"
+  },
+  {
+    "revision": "51ff819792a83d1ad715",
+    "url": "/maternity-landscape/static/js/2.15e933d6.chunk.js"
+  },
+  {
+    "revision": "96b85323f48b1c60dc5d",
+    "url": "/maternity-landscape/static/js/main.b9fca59d.chunk.js"
+  },
+  {
+    "revision": "dd8d815672d73d77bf36",
+    "url": "/maternity-landscape/static/js/runtime~main.c5eb4d52.js"
+  }
+]);
